c6062afecbbfd7420c26c39bff9756ce62e4b87d
Matheus P <matheuspn12@gmail.com>
2022-06-12 12:18:39 -0300

    Delete mo stuff

--- app/components/hello_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class HelloComponent < ViewComponent::Base
-  def initialize(text:)
-    @text = text
-  end
-
-end
--- app/controllers/dash/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Dash
-  class DashboardController < ApplicationController
-    def hello
-      render(HelloComponent.new(text: "Hello World"))
-    end
-  end
-end
